1ba5a77b52b83ee7fea3520b230c76b0145a22d3
Aldrin Azucena <43855657+aazucena@users.noreply.github.com>
2022-10-20 01:15:06 -0600

    added js files

---
+++ src/js/utilities/headers.js
@@ -0,0 +1,8 @@
+import directus from './services.js'
+import Big from 'big.js'
+
+class PitySystem {
+    constructor(props) {
+        this.bias = props.bias ?? 0
+    }
+}
---
+++ src/js/utilities/services.js
@@ -0,0 +1,5 @@
+import Directus from '@directus/sdk'
+
+const directus = new Directus("https://rmasdn6r.directus.app")
+
+export default directus
