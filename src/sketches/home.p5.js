82de5e5f9fbec24ed372533c1d12a5d32991c251
Aldrin Azucena <43855657+aazucena@users.noreply.github.com>
2022-10-26 22:48:38 -0600

    add more animations to the starting page

--- src/sketches/home.p5.js
+++ src/sketches/home.p5.js
@@ -1,46 +1,99 @@
 import fontColorContrast from 'font-color-contrast'
+import { fill } from 'lodash'
 
 var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 var vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
-
-
 export default (p5) => {
-    let x = 320
-    let y = 180
-    let xspeed = 5
-    let yspeed = 2
-    let r = 25
-    let button, buttonContainer
-    p5.setup = () => {
-      p5.createCanvas(vw, vh)
-      button = p5.createButton('<i class="icon f7-icons color-yellow">bitcoin</i>')
-      button.class('button button-circle button-large button-outline icon-button w-intial')
-      button.center('horizontal')
-      buttonContainer = p5.createDiv()
+
+    class ShowLight {
+      constructor(props) {
+        this.x = props.x 
+        this.y = props.y 
+        this.pivot = props.pivot
+        this.angle = p5.radians(props.angle)
+        this.offset = props.offset ?? 1
+        this.speed = (props.speed * p5.random(0, this.offset)) < 0.25 ? (props.speed * p5.random(0, this.offset)) : (props.speed * p5.random(0, this.offset)) * 0.1 
+        this.radius = props.radius
+        this.maxAngle = p5.radians(props.maxAngle ?? 180)
+        this.color = props.color ?? p5.color(255, 0, 0, 100)
+      }
+      move = () => {
+        p5.push()
+        p5.translate(this.x, this.y)
+        p5.rotate(this.angle)
+        p5.translate(-1 * this.pivot.x, -1 * this.pivot.y)
+        p5.fill(this.color)
+        p5.noStroke()
+        p5.drawingContext.filter = 'blur(1rem)'
+        p5.ellipseMode(p5.CENTER)
+        p5.ellipse(0, 0, this.radius, this.radius)
+        p5.pop()
+        this.angle += this.speed
+        if (this.angle >= this.maxAngle || this.angle <= 0)
+          this.speed *=  -1
+      }    
+    }
+    let show_lights = new Array(10).fill(null).map((_, i) => {
+      var rgb = new Array(3).fill(0)
+      rgb[i % 3] = 255
+      return new ShowLight({
+        x: vw/2 + (100 * p5.random(0, (i % 6))), 
+        y: vh/2 + 400 + (100 * (i % 6)), 
+        pivot: { x: 600, y: 460 }, 
+        angle: (i*45) % 90, 
+        speed: 0.0015,
+        maxAngle: 90, 
+        radius: 500, 
+        color: p5.color(...rgb, 25), 
+        offset: i + 1,
+      })
+    })
+    
+    const showLights = () => {
+      show_lights.forEach((show_light) => {
+        show_light.move()
+      })
+    }
+    const mainButton = () => {
+      let button = p5.createButton('<i class="icon f7-icons">chevron_right</i>')
+      button.class('button button-circle button-large color-secondary button-outline icon-button w-initial start-button')
+      // button.center('horizontal')
+      button.style('position', 'initial')
+      let buttonContainer = p5.createDiv()
       buttonContainer.child(button)
-      buttonContainer.class('w-100')
+      buttonContainer.class('w-100 display-flex flex-direction-row justify-content-center')
       buttonContainer.center()
       buttonContainer.position(0, p5.height/2+100)
     }
-    p5.draw = () => {  
-      p5.background('#1f203c')
+    const titleText = () => {
       p5.textSize(42)
       p5.textFont('Poppins', 36)
       p5.textStyle(p5.BOLD)
       p5.textAlign(p5.CENTER)
       p5.fill(fontColorContrast('#1f203c'))
       p5.text('Collective Assets', p5.width/2, p5.height/2 - 60)
-      p5.ellipse(x, y, r*2, r*2)
-      x += xspeed
-      y += yspeed
-      if (x > p5.width - r || x < r) {
-        xspeed = -xspeed
-      }
-      if (y > p5.height - r || y < r) {
-        yspeed = -yspeed
-      }
     }
+    const city = () => {
+      let image = p5.createImg('../assets/images/city.svg', 'city')
+      image.style('position', 'initial')
+      let imageContainer = p5.createDiv()
+      imageContainer.child(image)
+      imageContainer.class('w-100 display-flex flex-direction-row justify-content-center align-items-flex-end')
+      imageContainer.position(0, p5.height-151.75)
+    }
+    p5.setup = () => {
+      p5.createCanvas(vw, vh)
+      p5.background('#1f203c')
+      mainButton()
+      city()
+    }
+    p5.draw = () => {  
+      p5.background('#1f203c')
+      showLights()
+      titleText()
+    }
+
     p5.windowResized = () => {
       vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
       vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
