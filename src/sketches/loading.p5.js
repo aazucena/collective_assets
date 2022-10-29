35e0108f45c49b0254e9dcc850d090c33343cbf8
Aldrin Azucena <43855657+aazucena@users.noreply.github.com>
2022-10-29 01:49:13 -0600

    add loading page

--- src/sketches/loading.p5.js
+++ src/sketches/loading.p5.js
@@ -1,22 +1,53 @@
 import fontColorContrast from 'font-color-contrast'
 
+
+
 var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 var vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
 export default (p5) => {
     var counter = 0
-    p5.setup = () => {
-      p5.createCanvas(vw, vh)
-      p5.background('#4558ba')
+    var gif
+    p5.preload = () => {
+      gif = p5.createImg("../assets/images/coin-spin-small.gif")
     }
-    p5.draw = () => {  
-      p5.background('#4558ba')
+    const gifImage = () => {
+      gif.style('max-width', '100%')
+      gif.style('height', 'auto')
+      gif.size(75, 75)
+      let gifContainer = p5.createDiv()
+      gifContainer.child(gif)
+      gifContainer.class('w-100 display-flex flex-direction-row justify-content-center')
+      gifContainer.center()
+      gifContainer.position(0, (p5.height/2)-125)
+    }
+    const loadingTitle = () => {
+      p5.push()
       p5.textSize(42)
       p5.textFont('Poppins', 36)
       p5.textStyle(p5.BOLD)
       p5.textAlign(p5.CENTER)
       p5.fill(255, 255, 255)
-      p5.text(`Loading ${updateText()}`, p5.width/2, p5.height/2)
+      p5.text(`Loading ${updateText()}`, p5.width/2, (p5.height/2)+25)
+      p5.pop()
+    }
+    const loadingTips = () => {
+      p5.push()
+      p5.textFont('Poppins', 12)
+      p5.textAlign(p5.CENTER)
+      p5.fill(255, 255, 255)
+      p5.text(`Play as long as you like ;)`, p5.width/2, (p5.height)-50)
+      p5.pop()
+    }
+    p5.setup = () => {
+      p5.createCanvas(vw, vh)
+      p5.background('#4558ba')
+      gifImage()
+    }
+    p5.draw = () => {  
+      p5.background('#4558ba')
+      loadingTitle()
+      loadingTips()
     }
 
     p5.windowResized = () => {
@@ -25,7 +56,7 @@ export default (p5) => {
       p5.resizeCanvas(vw, vh)
     }
     const updateText = () => {
-      counter = counter < 3.5  ? counter + 0.05 : 0 
+      counter = counter < 3.5  ? counter + 0.00875 : 0 
       return new Array(p5.floor(counter)).fill('.').join(' ')
     }
 }
